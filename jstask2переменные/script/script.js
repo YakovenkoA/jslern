1d5059c881bbe17fd0cf3ef6f8f0af4093e42f48
YakovenkoA <aleksandrakovenko21@gmail.com>
2024-11-28 15:16:58 +0500

    js11

--- jstask2переменные/script/script.js
+++ jstask2переменные/script/script.js
@@ -1,3 +1,13 @@
-console.log('Привет'); 
-console.log('Hello,world!');
+let myLifeStule = 'freelance';
+console.log(myLifeStule);
 
+
+let myAge = 36;
+let myNewAge = 37;
+myAge = myNewAge;
+console.log(myNewAge);
+
+
+const myAgge = 38;
+//myAgge = 30; нельзя изменять
+console.log(myAgge);
