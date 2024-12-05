84b4b153f094f32b63bb94c546c85002e18ec8c5
“Name <aleksandrakovenko21@gmail.com>
2024-12-05 21:23:51 +0500

    type1

---
+++ jstask3типы_данных/script/script.js
@@ -0,0 +1,26 @@
+// underfined - неопределенный тип
+
+let userName; // определяем переменную
+// получаем тип данных
+console.log(typeof userName);
+// получаем значение переменной
+console.log(userName);
+
+
+///////////////////////////////////
+
+// Null           содержит только одно значение
+
+let useName = null;
+console.log(useName);
+
+//////////////////////////////////////////////
+
+// Boolean      true       false
+
+let willYouMe = true;
+if (willYouMe) {
+    console.log(':)');
+} else {
+    console.log(':(');
+}
